583a008625c356835260c18a1bd4e2ad04f9c603
sudhagar-g <sudhagarg449@gmail.com>
2025-08-11 23:18:09 +0530

    --

--- Anagram.py
+++ Anagram.py
@@ -1,14 +1,18 @@
-class Anagram :                        
-    def __init__(self,text):      
+class Anagram:
+    def __init__(self,text):
         self.text = text
+
+    def is_anagram(self):
         list1 = list(self.text)
-    def is_anagram(self):         
-        
         length = len(self.text)
         for i in range(length-1):
+            swapped = False
             for j in range(length -i - 1):
                 if list1[j] > list1[j+1]:
                     list1[j],list1[j+1] = list1[j+1],list1[j]
+                    swapped = True
+                if not swapped:
+                    break    
         return list1
 
 word1 = Anagram("madam")
