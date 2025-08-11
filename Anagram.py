53a41a65e9c55008e9447108d29e3d56afab92a5
sudhagar-g <sudhagarg449@gmail.com>
2025-08-11 23:09:04 +0530

    anagram without in-built sort methon and using bubble sort Algorithm

--- Anagram.py
+++ Anagram.py
@@ -1,9 +1,29 @@
-text1 = "sudhagar"
+class Anagram:
+    def __init__(self,text):
+        self.text = text
 
-word = list(text1)
+    def is_anagram(self):
+        list1 = list(self.text)
+        length = len(self.text)
+        for i in range(length-1):
+            for j in range(length -i - 1):
+                if list1[j] > list1[j+1]:
+                    list1[j],list1[j+1] = list1[j+1],list1[j]
+        return list1
+
+word1 = Anagram("madam")
+word2 = Anagram("mmaad")
 
 
-word.sort()
-print(word)
+if word1.is_anagram() == word2.is_anagram():
+    print(True)
+
+else:
+    print(False)    
 
 
+
+
+
+
+        
