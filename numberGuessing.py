c9a9b5ae1d1873943c0ed29e6aba3a62702d6ccb
sudhagar-g <sudhagarg449@gmail.com>
2025-08-14 10:50:28 +0530

    no changes

--- numberGuessing.py
+++ numberGuessing.py
@@ -11,15 +11,20 @@ while attempt < chance:
     try :
         my_guess = int(input("enter the number between 0 and 100 : ") )
         attempt +=1
+        
 
         if  my_guess < min_val or my_guess > max_val:
             print(f"please enter the number between {min_val} and {max_val}")
+            
+
+        
+            
         elif my_guess < random_number:
             print("Too low ")
         elif my_guess > random_number:
-            print("Too Hihg")
+            print("Too High")
         else:
-            print(f"congratulation you won the game and the number was {random_number} and guess the number in {attempt}th attempt")
+            print(f"congratulation you won the game and the number was {random_number} and you  guess the number in {attempt} th attempt")
             break
     except ValueError :
         print("  please enter the number") 
